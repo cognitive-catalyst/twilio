321a76ac2f0dd60e4d295187d8abbbaccc95621b
Sai Karthik Reddy Ginni <sai.ginni@us.ibm.com>
2016-06-14 15:54:20 -0400

    Updated the Navbar

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import header from 'img/nav-header.svg';
+import avatar from 'img/watson-avatar.svg';
 import './style.scss'
 
 export default ({children}) => (
     <div className='app'>
         {/*<a className='banner' href="http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/question-answer.html" target="_blank">Watson Demo</a>*/}
-        <img src={header} className='header'/>
+        <div className='header'>
+            <h2 className='header-text'>Watson Services on Twilio Marketplace Demo</h2>
+            <img src={avatar} className='avatar' />
+        </div>
         {children}
     </div>
 )
